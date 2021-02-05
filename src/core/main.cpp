4ba887f3315878222689aa8032b204d6934a74a2
volkalex28 <anan20000228@gmail.com>
2021-02-06 00:52:49 +0200

    TCP reconfig

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -10,21 +10,36 @@
  */
 
 #include <iostream>
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
 
-#include "main.hpp"
+extern "C"
+{
+  #include <stdlib.h> 
+  #include <string.h> 
+  #include <sys/socket.h> 
+  #include <sys/types.h> 
+  #include <unistd.h>
+  #include <netinet/in.h> 
+}
 
 using namespace std;
 
 const uint16_t PORT = 3390;
 const uint16_t MAXSYM = 256;
+
+void foo(int sockfd)
+{
+  char buffer[MAXSYM] = {0};
+  while(true)
+  {
+    int n = recv(sockfd, buffer, MAXSYM, 0); 
+    cout << "Client : " << buffer; 
+
+    n = write(sockfd, buffer, strlen(buffer)); 
+    cout << "Message sent." << endl << endl; 
+
+    memset(buffer, 0, MAXSYM);
+  }
+}
 
 /**
  * @brief 
@@ -35,18 +50,15 @@ const uint16_t MAXSYM = 256;
  */
 int main(int argc, char *argv[])
 {
-  char buffer[MAXSYM] = {0};
   sockaddr_in serv, cli;
 
   // Creating socket file descriptor 
-  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if(sockfd < 0)
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sockfd == -1)
   { 
     perror("socket creation failed"); 
     exit(EXIT_FAILURE); 
   }
-
-  memset(&serv, 0, sizeof(serv)); 
 
   // Filling server information 
   serv.sin_family       = AF_INET; // IPv4 
@@ -54,23 +66,34 @@ int main(int argc, char *argv[])
   serv.sin_port         = htons(PORT);
 
   // Bind the socket with the server address 
-  if (bind(sockfd, (const sockaddr*)&serv, sizeof(serv)) < 0) 
+  if (bind(sockfd, (sockaddr*)&serv, sizeof(serv)) < 0) 
   { 
-    perror("bind failed"); 
+    perror("socked bind failed"); 
     exit(EXIT_FAILURE); 
   }
 
-
-  while(true)
+  // Now server is ready to listen and verification
+  if(listen(sockfd, 5) != 0)
   {
-    socklen_t len = sizeof(cli);  //len is value/resuslt 
-    int n = recvfrom(sockfd, (char*)buffer, MAXSYM, MSG_WAITALL, (sockaddr*)&cli, &len); 
-    buffer[n] = '\0'; 
-    cout << "Client : " << buffer; 
-
-    sendto(sockfd, (const char*)buffer, strlen(buffer), MSG_CONFIRM, (const sockaddr*) &cli, len); 
-    cout << "Message sent." << endl << endl; 
+    perror("listen failed"); 
+    exit(EXIT_FAILURE); 
   }
+
+  int len = sizeof(cli);  //len is value/resuslt 
+  if(accept(sockfd, (sockaddr*)&cli, (socklen_t*)&len) < 0)
+  {
+    perror("server accept failed"); 
+    exit(EXIT_FAILURE); 
+  }
+  else
+  {
+    cout << "Server accept the client" << endl;
+  }
+
+  foo(sockfd);
+
+  close(sockfd);
+  cout << "end";
 
   return 0;
 }
